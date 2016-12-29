9779af862e60bc999823668fbc399c8c8719054b
Jorn Heyvaert <jorn.heyvaert@telenet.be>
2016-12-29 14:27:13 +0100

    Initial commit

--- KochCurve/Form1.Designer.cs
+++ KochCurve/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace KochCurve
 {
-    partial class Form1
+    partial class KochForm
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,59 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.btnDraw = new System.Windows.Forms.Button();
+            this.lblDepth = new System.Windows.Forms.Label();
+            this.txtDepth = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnDraw
+            // 
+            this.btnDraw.Location = new System.Drawing.Point(479, 86);
+            this.btnDraw.Name = "btnDraw";
+            this.btnDraw.Size = new System.Drawing.Size(182, 63);
+            this.btnDraw.TabIndex = 0;
+            this.btnDraw.Text = "Draw";
+            this.btnDraw.UseVisualStyleBackColor = true;
+            this.btnDraw.Click += new System.EventHandler(this.btnDraw_Click);
+            // 
+            // lblDepth
+            // 
+            this.lblDepth.AutoSize = true;
+            this.lblDepth.Location = new System.Drawing.Point(372, 9);
+            this.lblDepth.Name = "lblDepth";
+            this.lblDepth.Size = new System.Drawing.Size(289, 13);
+            this.lblDepth.TabIndex = 1;
+            this.lblDepth.Text = "Depth: (Enter value between 0 and 5 (boundaries included))";
+            // 
+            // txtDepth
+            // 
+            this.txtDepth.BackColor = System.Drawing.SystemColors.Window;
+            this.txtDepth.Location = new System.Drawing.Point(479, 44);
+            this.txtDepth.Name = "txtDepth";
+            this.txtDepth.Size = new System.Drawing.Size(182, 20);
+            this.txtDepth.TabIndex = 2;
+            this.txtDepth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDepth_KeyPress);
+            // 
+            // KochForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(673, 378);
+            this.Controls.Add(this.txtDepth);
+            this.Controls.Add(this.lblDepth);
+            this.Controls.Add(this.btnDraw);
+            this.Name = "KochForm";
+            this.Text = "KochCurve";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Button btnDraw;
+        private System.Windows.Forms.Label lblDepth;
+        private System.Windows.Forms.TextBox txtDepth;
     }
 }
 
--- KochCurve/Form1.cs
+++ KochCurve/Form1.cs
@@ -1,20 +1,82 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace KochCurve
 {
-    public partial class Form1 : Form
+    public partial class KochForm : Form
     {
-        public Form1()
+        private bool isPainted = false;
+
+        public KochForm()
         {
             InitializeComponent();
+        }
+
+        private void btnDraw_Click(object sender, EventArgs e)
+        {
+            Graphics g = this.CreateGraphics();
+            if (isPainted)
+            {
+                g.Clear(this.BackColor);
+            }
+            if (!string.IsNullOrEmpty(txtDepth.Text))
+            {
+                int depth = Convert.ToInt32(txtDepth.Text);
+                if (depth > -1 && depth < 6) // depth must be between 0 and 5
+                {
+                    Pen pen = new Pen(Color.FromArgb(255, 0, 0, 0));
+                    drawSnow(g, pen, depth, 20, 280, 280, 280);
+                    drawSnow(g, pen, depth, 280, 280, 150, 20);
+                    drawSnow(g, pen, depth, 150, 20, 20, 280);
+                    isPainted = true;
+                    txtDepth.BackColor = SystemColors.Window;
+                }
+                else
+                {
+                    txtDepth.BackColor = Color.Tomato;
+                }
+            }
+            else
+            {
+                txtDepth.BackColor = Color.Tomato;
+            }
+        }
+
+        // To allow only numbers in a textbox in a windows application and backspace to remove previously entered text
+        private void txtDepth_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!(Char.IsDigit(e.KeyChar) || (e.KeyChar == (char)Keys.Back)))
+                e.Handled = true;
+        }
+
+        private void drawSnow(Graphics g, Pen p, int lev, int x1, int y1, int x5, int y5)
+        {
+            int deltaX, deltaY, x2, y2, x3, y3, x4, y4;
+
+            if (lev == 0)
+            {
+                g.DrawLine(p, x1, y1, x5, y5);
+            }
+            else
+            {
+                deltaX = x5 - x1;
+                deltaY = y5 - y1;
+
+                x2 = x1 + deltaX / 3;
+                y2 = y1 + deltaY / 3;
+
+                x3 = (int)(0.5 * (x1 + x5) + Math.Sqrt(3) * (y1 - y5) / 6);
+                y3 = (int)(0.5 * (y1 + y5) + Math.Sqrt(3) * (x5 - x1) / 6);
+
+                x4 = x1 + 2 * deltaX / 3;
+                y4 = y1 + 2 * deltaY / 3;
+
+                drawSnow(g, p, lev - 1, x1, y1, x2, y2);
+                drawSnow(g, p, lev - 1, x2, y2, x3, y3);
+                drawSnow(g, p, lev - 1, x3, y3, x4, y4);
+                drawSnow(g, p, lev - 1, x4, y4, x5, y5);
+            }
         }
     }
 }
